5404b4b17fd53f1ddfd7be7ad0cb9b3d557bab08
P4NAAA2007 <110509026+P4NAAA2007@users.noreply.github.com>
2022-08-07 18:51:15 -0700

    Add files via upload

---
+++ python1.py
@@ -0,0 +1,11 @@
+#Belajar OUTPUT
+print("hello World")
+print("Nama Saya Maulana")
+print("Saya Lahir Di Jakarta")
+print("Saya Berumur 15 Tahun")
+print("Saya Tinggal Di Kota Depok")
+print("Alamat Rumah Saya")
+print("Berada Di Jl.Kramat Benda 2 ")
+print("RT 05/28")
+print("Kel.Baktijaya Kec.Sukmajaya")
+print("Saya Mempunyai Hoby Bermain Futsal")
---
+++ python2.py
@@ -0,0 +1,13 @@
+#Belajar Input
+
+nama = "Lana" # Lana adalah Data, Nama Adalah Variabel
+umur = 15
+alamat = "Baktijaya,Kota Depok"
+
+print(nama)
+print(umur)
+print(alamat)
+
+
+
+print("Nama saya adalah " + nama)
