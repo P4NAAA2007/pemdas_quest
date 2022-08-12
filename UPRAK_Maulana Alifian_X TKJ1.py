905b3b0a384cecf830b451cf4d9337070b75acd3
P4NAAA2007 <110509026+P4NAAA2007@users.noreply.github.com>
2022-08-11 20:52:58 -0700

    Add files via upload

---
+++ UPRAK_Maulana Alifian_X TKJ1.py
@@ -0,0 +1,11 @@
+nama =input("masukkan nama anda")
+nama_depan =input("nama depan anda")
+
+print("nama anda adalah"+ nama_depan + nama )
+
+
+
+
+suhu = input("masukkan suhu celcius")
+reamur = ( 4/5 * suhu)
+print (suhu+ "suhu celcius adalah"+ reamur + "reamur")
